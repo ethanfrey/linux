5b0b5122a0f66ec756279141e0860cdd51c30893
Ethan Frey <ethanfrey@users.noreply.github.com>
2017-10-27 18:03:28 +0200

    Update apt and golang

--- packages.sh
+++ packages.sh
@@ -1,3 +1,8 @@
 apt-get install -y golang-1.9-go
 apt-get install -y git jq curl
+
+# editors
 apt-get install -y terminator nautilus sublime-text-installer code
+
+# for nvm
+apt-get install -y build-essential checkinstall libssl-dev
